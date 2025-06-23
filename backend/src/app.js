1cc29ab6cfa5a9e959b1710c4429827dcbc3b4cc
Eduardo Dourado <ed320819@gmail.com>
2025-06-22 22:18:36 -0300

    code cleanup and minor fixes

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../../public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 
 app.use('/api', apiRouter);
